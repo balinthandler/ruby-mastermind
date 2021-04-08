dd949517d32f910f43edc15bcd4ec63f322bcef6
Balint Handler <handlerbalint@gmail.com>
2021-04-08 21:04:57 +0200

    random code generating, basic game methods, input for guesses

--- script.rb
+++ script.rb
@@ -13,7 +13,8 @@ require 'colorize'
 # CODEBREAKER GUESSES UNTIL HE WINS OR RUNS OUT OF GUESSING
 
 module Colors
-  @colors = ["red", "green", "blue", "yellow", "orange", "purple"]
+  #colors array for red, green, blue, yellow, magenta, orange
+  @colors = ["r", "g", "b", "y", "m", "o"]
 
   def self.get_colors
     puts @colors
@@ -51,8 +52,8 @@ module Intro
   def self.get_role
     loop do
       puts "\nHello #{@name},"
-      sleep 1
-      puts "to play as CodeBreaker, type \"B\", or to play as CodeMaker, type \"M\", and press ENTER!"
+      sleep 0.5
+      puts "to play as CodeBreaker, type \"B\", or to play as CodeMaker, type \"M\", and press ENTER! "
       role = gets.chomp.upcase
       if role == "B" || role == "M"
         return role
@@ -63,41 +64,110 @@ module Intro
   def self.show_instructions
     puts "\n--Creating code--"
     sleep 0.5
-    puts "    ? ? ? ?"
-    sleep 0.2
-    puts "--Code created--"
+    print "    "
+    4.times do
+      print " ?" 
+      sleep 0.4 
+    end
+    puts "\n--Code created--"
     sleep 1
-    puts "Try to guess the colors in the 4 slots!"
+    puts "Try to guess the colors and their order in the four slots above! "
     puts "Available colors are: "     + " Red ".colorize(:background => :light_red) + " " +
       " Green ".colorize(:background => :light_green) + " " +
       " Blue ".colorize(:background => :blue) + " " +
       " Yellow ".colorize(:background => :light_yellow) + " " +
       " Magenta ".colorize(:background => :light_magenta) + " " +
       " Orange ".colorize(:background => :yellow)
-    puts "One color could take multiple spots."
-    puts "To guess, type only the initials, e.g. R for red, G for green etc.."
-    puts "You have 12 turns to guess!"
-    puts "Enter a pattern like this: G R B Y"
+    puts "One color could take multiple slots."
+    puts "To guess, type only the initials, e.g. R for red, G for green etc.. "
+    puts "Enter a pattern similar to this: RRBY. "
+    puts "Which means your guess is Red Red Blue Yellow. "
+    puts "The computer will give you feedback with 2 flags: "
+    puts "White flag".colorize(:background => :light_white, :color => :black) + ": the color is correct, but it is on the wrong spot. "
+    puts "Red flag".colorize(:background => :light_red, :color => :black) + ": the color is correct and it is on the right spot. "
+    puts "You only got 3 flags, that means the fourth color is incorrect."
+    puts "For example you get " + "R".colorize(:background => :light_red, :color => :black) + " " + 
+    "W".colorize(:background => :light_white, :color => :black) + " " + 
+    "W".colorize(:background => :light_white, :color => :black)
+    puts "Which means: 1 color is correct and is on the right spot, "
+    puts "and 2 other color also correct, but they are on the wrong spots."
+    puts "Beware! Feedback flags do not indicate positions! So on the example above "
+    puts "you don't know which of your color has got red or white or no flag."
+    puts "You have 12 turns to guess and to win! "
+    puts "Let's start!"
+
   end
 end
 
-module Guess
+class Guess
+  def initialize
+    @guess = gets.chomp.downcase
+  end
+
+  def check_guess(code,guess)
+    flags = []
+    binding.pry
+    for i in 0..3 do
+      if guess[i] == code[i]
+        flags.push("red")
+      else
+        for j in 0..3
+          if j == i 
+            next
+          end
+          if guess[i] == code[j]
+            flags.push("white")
+          end
+        end
+        return flags
+      end
+    end
+  end
+
+  def value
+    return @guess
+  end
+end
+
+module GameMethods
+  def self.start_game
+    @game_over = false
+    Intro.welcome
+    player = Intro.get_player
+    role = Intro.get_role
+    # @code = Colors.pick_random
+    Intro.show_instructions
+  end
+  
   def self.get_guess
-    guess = gets.chomp.gsub(" ", "").upcase
-    return guess
+    #for testing------
+    @code = ["r","g","b","b"]
+    #for testing------
+    puts "\nYour guess is:"
+    @guess = Guess.new
+    # binding.pry
+    until @guess.value.match(/[rgbymo]{4}/)
+      puts "\nGive valid colors like GBYY or RMBG!".colorize(:color => :light_red)
+      @guess = Guess.new
+    end
+    return @guess
   end
 
   
+  def self.evaluate_guess
+    if @guess.check_guess(@code,@guess.value)
+      puts "dam, you are good"
+    else
+      puts "nope"
+    end
+  end
+  
+  def show_result
+
+  end
 end
-def game
-  Intro.welcome
-  player = Intro.get_player
-  role = Intro.get_role
-  code = Colors.pick_random
-  Intro.show_instructions
 
-
-end
-
-game()
+#GameMethods.start_game()
+GameMethods.get_guess()
+GameMethods.evaluate_guess()
 
